bc5400104384c29889cf88fca3e92f4cf5363294
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2023-04-17 23:54:04 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,11 @@
 export default function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  ).then(data => data.json());
+  ).then(data => {
+      if (data.ok) {
+          return data.json();
+      } else {
+          throw new Error(data.status);
+      }
+  });
 }
