198cbcd82fccd61f3c9ad0006457f26cb9439895
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2023-04-17 23:53:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,42 +15,45 @@ refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(evt) {
   const inputValue = evt.target.value.trim();
-  cleanList();
-  cleanInfo();
+    cleanMarkup(refs.ulEl);
+    cleanMarkup(refs.divEl);
   if (inputValue != '') {
     fetchCountries(inputValue)
-      .then(flags => {
-        if (flags.status === 404) throw new Error('No data!');
-        if (flags.length > 10) {
+        .then(countries => {
+        if (countries.length > 10) {
           Notify.info(
             'Too many matches found. Please enter a more specific name.'
           );
-          cleanList();
+        cleanMarkup(refs.ulEl);
           return;
         }
-        const markupList = flags.reduce(
-          (markup, flag) => markup + createMarkupCountry(flag),
+        const markupList = countries.reduce(
+          (markup, country) => markup + createMarkupCountry(country),
           ''
         );
         updateCountry(markupList, refs.ulEl);
-        if (flags.length === 1) {
+        if (countries.length === 1) {
           const h2El = document.querySelector('.country-title');
           h2El.classList.add('country-title__biggest');
-          const markupInfo = createMarkupInfoCountry(flags[0]);
+          const markupInfo = createMarkupInfoCountry(countries[0]);
           updateCountry(markupInfo, refs.divEl);
         } else {
-          cleanInfo();
+            cleanMarkup(refs.divEl);
         }
       })
-      .catch(onError);
+        .catch(error => {
+        if (error.message === '404')
+          Notify.failure('Oops, there is no country with that name');
+       
+        onError(error);
+      });
   }
 }
 
 function onError(err) {
-  Notify.failure('Oops, there is no country with that name');
-  cleanInfo();
-  cleanList();
-  cleanInput();
+    cleanMarkup(refs.divEl);
+    cleanMarkup(refs.ulEl);
+    cleanInput();
   console.error(err);
 }
 
@@ -76,12 +79,8 @@ function updateCountry(markup, elem) {
   elem.innerHTML = markup;
 }
 
-function cleanList() {
-  refs.ulEl.innerHTML = '';
-}
-
-function cleanInfo() {
-  refs.divEl.innerHTML = '';
+function cleanMarkup(elem) {
+  elem.innerHTML = '';
 }
 
 function cleanInput() {
